295c86dd88cc61ae1d8b486ee5db534c2171ce90
ferlobo1985 <fernando.lobo.prez@gmail.com>
2020-11-25 16:26:05 -0300

    a better layout

--- src/App.js
+++ src/App.js
@@ -9,18 +9,24 @@ const App = () => {
   return (
     <BrowserRouter>
         <header>
-          <Link to="/">Home</Link> -
-          <Link to="/posts">Posts</Link> - 
-          <Link to={{
-            pathname:'/profile',
-            hash:'#francis',
-            search:'?true=enabled'
-          }}>Profile</Link>
-          <hr/>
+          <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+            <h5 className="my-0 mr-md-auto font-weight-normal">Company name</h5>
+            <nav className="my-2 my-md-0 mr-md-3">
+              <Link className="p-2 text-dark" to="/">Home</Link> -
+              <Link className="p-2 text-dark" to="/posts">Posts</Link> - 
+              <Link className="p-2 text-dark" to={{
+                pathname:'/profile',
+                hash:'#francis',
+                search:'?true=enabled'
+              }}>Profile</Link>
+            </nav>
+          </div>
         </header>
-        <Route path="/" exact component={Home}/>
-        <Route path="/posts" exact component={Post}/>
-        <Route path="/profile" exact component={Profile}/>
+        <div className="container">
+          <Route path="/" exact component={Home}/>
+          <Route path="/posts" exact component={Post}/>
+          <Route path="/profile" exact component={Profile}/>
+        </div>
     </BrowserRouter>
   );
 }
