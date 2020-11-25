f33594c601f8bebd5034d0e3a188237d657d948e
ferlobo1985 <fernando.lobo.prez@gmail.com>
2020-11-25 15:20:37 -0300

    react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter,Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './components/home';
 import Post from './components/posts';
@@ -7,9 +7,14 @@ import Profile from './components/profile';
 
 const App = () => {
   return (
-    <>
-      Hello
-    </>
+    <BrowserRouter>
+        <header>
+          header
+        </header>
+        <Route path="/" exact component={Home}/>
+        <Route path="/posts" component={Post}/>
+        <Route path="/profile" component={Profile}/>
+    </BrowserRouter>
   );
 }
 
